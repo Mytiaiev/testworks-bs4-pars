7bf4c0c36a526977bd6f8776cca1c15011bbefbc
Mytiaiev <90474070+Mytiaiev@users.noreply.github.com>
2022-08-23 12:18:33 +0300

    Update common.py

--- common.py
+++ common.py
@@ -1,12 +1,12 @@
 import telebot
 
 
-TOKEN = "5457793866:AAGvpAkj0h6wYKoJNMpQiLgmjU6DpVMqEVE"
-chat_id = '-619051510'
+TOKEN = "*"
+chat_id = '*'
 tb = telebot.TeleBot(TOKEN)
 login_data = {
-    'username': 'mytiaievqa@gmail.com',
-    'password': 'qwerty=1'
+    'username': '*',
+    'password': '*'
 }
 login_url = 'https://www.tesmanian.com/account'
 
